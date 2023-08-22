e68900e30b8a0ef7b6b0a83ef3afef262de57b8b
Nesha Mervin <nesha.mervin@gmail.com>
2023-08-22 13:41:45 -0400

    added built-in middleware to automatically serve up css and javascript files in /public folder

--- server.js
+++ server.js
@@ -16,6 +16,7 @@ MongoClient.connect(dbConnectionStr, { useUnifiedTopology: true })
   .catch(err => console.error(err));
 
 app.set('view engine', 'ejs');
+app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
