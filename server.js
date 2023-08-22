768c21968841a93c48380bb9e4f87d74f267c263
Nesha Mervin <nesha.mervin@gmail.com>
2023-08-21 20:10:57 -0400

    installed mongodb and connected to database

--- server.js
+++ server.js
@@ -1,11 +1,30 @@
 const express = require('express');
 const app = express();
+const MongoClient = require('mongodb').MongoClient;
 const PORT = 8000;
+require('dotenv').config();
+
+let db,
+  dbConnectionStr = process.env.DB_STRING,
+  dbName = 'toDos';
+
+MongoClient.connect(dbConnectionStr, { useUnifiredTopology: true })
+  .then(client => {
+    console.log(`Connected to ${dbName} Database`);
+    db = client.db(dbName);
+  })
+  .catch(err => console.error(err));
 
 app.set('view engine', 'ejs');
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 app.get('/', (req, res) => {
   res.render('index.ejs');
+});
+
+app.post('/todos', (req, res) => {
+  console.log(req.body.todo);
 });
 
 app.listen(PORT, () => {
