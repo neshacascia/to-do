ec777b1e0329be7e7449328df6df2fc88a432006
Nesha Mervin <nesha.mervin@gmail.com>
2023-08-23 12:04:58 -0400

    used Array.from method to create an array from the NodeList of delete buttons

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,8 @@
 const deleteBtn = document.querySelectorAll('#deleteToDo');
 
-deleteBtn.forEach(elem => addEventListener('click', deleteToDo));
+Array.from(deleteBtn).forEach(elem =>
+  elem.addEventListener('click', deleteToDo)
+);
 
 async function deleteToDo() {
   console.log('Delete button clicked!');
