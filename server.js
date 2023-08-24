9d5a38b9e3b2c8d64250f7f4e75c41abb505e582
Nesha Mervin <nesha.mervin@gmail.com>
2023-08-24 11:53:33 -0400

    removed cors

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const app = express();
-const cors = require('cors');
 const MongoClient = require('mongodb').MongoClient;
 const { ObjectId } = require('mongodb');
 const PORT = 8000;
@@ -16,7 +15,6 @@ MongoClient.connect(dbConnectionStr, { useUnifiedTopology: true })
     db = client.db(dbName);
 
     app.set('view engine', 'ejs');
-    app.use(cors());
     app.use(express.static('public'));
     app.use(express.urlencoded({ extended: true }));
     app.use(express.json());
