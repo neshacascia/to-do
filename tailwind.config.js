25076c41e7083faf5c3b675edcfd6ae73cd02af3
Nesha Mervin <nesha.mervin@gmail.com>
2023-08-24 10:01:11 -0400

    added styling for larger screens

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,7 +10,7 @@ module.exports = {
         completed: '#D1D2DA',
       },
       backgroundImage: {
-        bgMobileLight: "url('/images/bg-mobile-light.jpg')",
+        bgDesktop: "url('/images/bg-desktop.png')",
       },
       fontFamily: {
         body: ['Josefin Sans'],
