8a390d67e3f262749bb4386bcc847f8fd1018871
Nesha Mervin <nesha.mervin@gmail.com>
2023-08-23 21:47:12 -0400

    when toDo spans are clicked, markCompleted function executes, grabbing the id of the item clicked and sending it to server as a put request

--- public/js/main.js
+++ public/js/main.js
@@ -1,8 +1,31 @@
+const toDos = document.querySelectorAll('#toDo');
 const deleteBtn = document.querySelectorAll('#deleteToDo');
 
 Array.from(deleteBtn).forEach(elem =>
   elem.addEventListener('click', deleteToDo)
 );
+
+Array.from(toDos).forEach(elem =>
+  elem.addEventListener('click', markCompleted)
+);
+
+async function markCompleted() {
+  const toDoId = this.parentNode.dataset.id;
+
+  try {
+    const res = await fetch('/markCompleted', {
+      method: 'put',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ toDo: toDoId }),
+    });
+
+    const data = await res.json();
+    console.log(data);
+    location.reload();
+  } catch (err) {
+    console.error(err);
+  }
+}
 
 async function deleteToDo() {
   const toDoId = this.parentNode.dataset.id;
