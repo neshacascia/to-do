d649180fee6bb08940b4c18e55b54525fd0c2909
Nesha Mervin <nesha.mervin@gmail.com>
2023-08-29 09:35:39 -0400

    go into homeRoutes when router is on the root route

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const connectDB = require('./config/database');
+const homeRoutes = require('./routes/home');
 
 require('dotenv').config({ path: './config/.env' });
 
@@ -10,6 +11,8 @@ app.set('view engine', 'ejs');
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+app.use('/', homeRoutes);
 
 app.listen(process.env.PORT, () => {
   console.log(`The server is running on port ${process.env.PORT}.`);
